596aa1a1edc8db4382fb5dac7b6269bce19a723c
Anant <anantji2332@gmail.com>
2025-09-20 17:06:06 +0530

    Farmer:Login Page Integrated

--- Farmer/src/App.jsx
+++ Farmer/src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
 import CropCalendar from "./pages/CropCalender";
 import OrganicGuide from "./pages/OrganicFarmingGuide";
 import DiseaseClassifier from "./pages/DiseaseClassifier";
@@ -25,6 +26,7 @@ const App = () => (
         <Navbar/>
           <Routes>
             <Route path="/" element={<Index />} />
+            <Route path="/login" element={<Login />} />
             <Route path="/calendar" element={< CropCalendar/>} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />

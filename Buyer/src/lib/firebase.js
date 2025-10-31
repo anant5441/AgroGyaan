bf8bc61a7d05e49bbe4f873ddf29c49fa16fcad5
ishan-saraswat-68 <ishansaraswat68@gmail.com>
2025-11-01 01:32:11 +0530

    Buyer: bug-fix

--- Buyer/src/lib/firebase.js
+++ Buyer/src/lib/firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -24,5 +23,3 @@ export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
 
-
-const analytics = getAnalytics(app);
